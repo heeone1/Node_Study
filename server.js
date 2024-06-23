12b1c3fcedb3fa8d4ce8eaf459a0244e2e8c9123
heeone1 <rinakim1019@gmail.com>
2024-06-24 04:24:24 +0900

    로그인, 회원가입

--- server.js
+++ server.js
@@ -9,6 +9,65 @@ app.use(express.urlencoded({ extended: true }));
 
 const { MongoClient, ObjectId } = require('mongodb');
 
+// //로그인
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+//해싱
+const bcrypt = require("bcrypt");
+
+//DB에 로그인정보 저장
+const MongoStore = require('connect-mongo');
+
+app.use(passport.initialize())
+app.use(session({
+	secret : "1234", //암호화에 쓸 비밀번호 
+	resave : false, //유저가 로그인 안해도 세션을 저장해둘지 여부
+	saveUninitialized : false // 유저가 요청할 때 마다 session데이터를 다시 갱신할지
+}))
+app.use(passport.session())
+
+//DB에 로그인정보 저장
+app.use(session({
+    resave : false,
+    saveUninitialized : false,
+    secret: "1234", //세션 암호화 비밀번호
+    cookie : {maxAge : 1000 * 60},
+    store: MongoStore.create({
+      mongoUrl : "mongodb+srv://admin:qwer1234@cluster0.fretgqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      dbName: "forum",
+    })
+  })) 
+  
+
+passport.use(new LocalStrategy(async (id,pw,cb) => {
+	let result = await db.collection("user").findOne({username : id})
+	if (!result) {
+		return cb(null, false, {message : "ID가 DB에 없습니다"})
+	}
+	if (await bcrypt.compare(pw, result.password)) {
+		return cb(null, result)
+	} else {
+		return cb(null, false, {message : "PW가 일치하지 않습니다"});
+	}
+}))
+
+passport.serializeUser((user, done) => {
+	process.nextTick(() => {
+		done(null, {id : user._id, username : user.username})
+	})
+})
+passport.deserializeUser(async (user,done) => {
+	let result = await db
+        .collection("user")
+        .findOne({_id : new ObjectId(user.id)});
+	delete result.password;
+	process.nextTick(() => {
+		return done(null, result)
+	})
+})
+
 let db;
 const url = 
     "mongodb+srv://admin:qwer1234@cluster0.fretgqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -42,13 +101,13 @@ app.get("/baseball", (req, res) => {
 
 //db에 있는 데이터 꺼내기
 //await db.collection('컬렉션명').find().toArray()
+// app.get("/list", async(req, res) => {
+//     let result = await db.collection("post").find().toArray()
+//     res.send(result[0].title)
+// });
+
+
 app.get("/list", async(req, res) => {
-    let result = await db.collection("post").find().toArray()
-    res.send(result[0].title)
-});
-
-
-app.get("/listejs", async(req, res) => {
     let result = await db.collection("post").find().toArray()
     res.render("list.ejs", {posts: result});
 });
@@ -94,3 +153,56 @@ app.post("/edit", async (req, res) => {
       );
     res.redirect("/listejs");
 });
+
+app.delete("/delete", async (req, res) => {
+    try {
+      let result = await db
+        .collection("post")
+        .deleteOne({ _id: new ObjectId(req.query.docid) });
+      if (result.deletedCount === 1) {
+        res.send("삭제완료");
+      } else {
+        res.send("삭제 실패: 해당 문서를 찾을 수 없습니다.");
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("삭제 중 오류 발생");
+    }
+  });
+
+
+app.get("/list/:id", async (req, res)=>{
+	let result = await db.collection("post").find()
+		.skip((req.params.id-1)*5).limit(5).toArray();
+	res.render("list.ejs", {posts : result})
+})
+
+
+//로그인
+app.get('/login', (req, res)=>{
+	res.render("login.ejs")
+})
+
+app.post("/login", async (req,res,next) => {
+	passport.authenticate("local", (error, user, info)=>{
+		if (error) return res.status(500).json(error)
+		if (!user) return res.status(401).json(info.message)
+		req.logIn(user, (err) => {
+			if (err) return next(err)
+			res.redirect("/list")
+		})
+	}) (req,res,next)
+})
+
+//회원가입
+app.get("/register", (req,res)=>{
+	res.render("register.ejs");
+})
+app.post("/register", async (req,res) => {
+	let hash = await bcrypt.hash(req.body.password, 10)
+	await db.collection("user").insertOne({
+		username : req.body.username,
+		password : hash
+	});
+	res.redirect("/login");
+})
